b6e0ebe68d0693dc1b326b07e686dd9bb933068e
Parsa Hosni <parsa.hsn78@gmail.com>
2022-10-20 00:46:49 +0330

    Auth Files Created

--- server/index.js
+++ server/index.js
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+dotenv.config();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
+    cb(null, "./public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
@@ -26,8 +28,7 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
   res.status(200).json(file.filename);
 });
 
-
-dotenv.config();
+app.use('/api/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Connected on Port ${process.env.PORT}`);
