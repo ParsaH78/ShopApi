77aba9a4f5b27f169beb5f7a7079f2b00629ca43
Parsa Hosni <parsa.hsn78@gmail.com>
2022-10-24 20:55:43 +0330

    Create Cart return updated

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -11,6 +11,7 @@ export const createCart = (req, res) => {
 
     db.query(query, [Object.values(cartInfo)], (error, data) => {
         if (error) return res.status(500).json(error);
+        return res.status(200).json("Cart has been Created");
     });
 
     // query = `UPDATE products SET print_length = products.print_length - ? WHERE id = ? ;`;
