4c32f763942812a2f81b3f3c52ee576722095c82
Parsa Hosni <parsa.hsn78@gmail.com>
2022-10-25 22:40:46 +0330

    User Controller Updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import { db } from "../database.js";
 
 export const updateUser = (req, res) => {
-    const {id, ...userInfo} = req.body;
+    const {user_id, ...userInfo} = req.body;
 
     const keys = Object.keys(userInfo);
     const values = Object.values(userInfo);
@@ -19,12 +19,12 @@ export const updateUser = (req, res) => {
         }
     }
 
-    query = query.concat(` WHERE id = ${req.body.id} ;`);
+    query = query.concat(` WHERE id = ${req.user} ;`);
 
     db.query(query,
          (error, data) => {
             if (error) return res.status(404).json(error);
-            res.status(201).json({message: "بروزرسانی کاربر با موفقیت انجام شد"});
+            res.status(201).json({message: "User has been Updated"});
      });
 };
 
@@ -33,7 +33,7 @@ export const getUser = (req, res) => {
     const query = `SELECT first_name, last_name, email, username, image, 
         address, city, postal_code, phone_number from users WHERE id = ? ;`
 
-    db.query(query, [req.user.id], (error, data) => {
+    db.query(query, [req.user], (error, data) => {
         if (error) return res.status(404).json(error);
         return res.status(200).json(data[0]);
     });
@@ -45,7 +45,7 @@ export const getUserOrders = (req, res) => {
     const query = `SELECT product_id, product_quantity, price, address, status
         From orders JOIN users ON orders.user_id = users.id WHERE users.id = ?`;
 
-    db.query(query, [req.user.id], (error, data) => {
+    db.query(query, [req.user], (error, data) => {
         if (error) return res.status(404).json(error);
         return res.status(200).json(data[0]);
     })
