7e9aa64a331c3510c7f3040dc6d0805b7b7bc50a
Parsa Hosni <parsa.hsn78@gmail.com>
2022-10-26 12:52:58 +0330

    AuthMiddleware updated

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ export const protect = async (req, res, next) => {
   ) {
     try {
       // Get token from header
-      token = req.headers.authorization.split(' ')[1];
+      token = req.cookies.access_token;
 
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_KEY);
